1655393c413b8ba247ab6ad120f91f7bfa68f13c
Your Name <your.email@example.com>
2026-01-05 08:23:43 +0100

    Final Final Final sync!

--- Javascript/Login.js
+++ Javascript/Login.js
@@ -14,8 +14,8 @@ document.addEventListener('DOMContentLoaded', function() {
         const news1 = document.getElementById('news1');
         const news2 = document.getElementById('news2');
         if (news1 && news2) {
-            news1.textContent = 'Now that 2025 is over we can look at some data. An estimated  38.1 billion tonnes of carbon dioxide has been released into the atmosphere as of 2025. As many already know going at such a pace is not sustainable, but where will we go to fix this is the ultimate question. https://globalcarbonbudget.org/fossil-fuel-co2-emissions-hit-record-high-in-2025/ ';
-            news2.textContent = 'Mechanical exoskeletons are being made to help everyday people who suffered from stroke, regain motor fuctions. This technology has been developed in Norway and asking the test subjects they are quite positive on their effectiveness. https://www.goodnewsnetwork.org/it-feels-like-me-again-the-worlds-first-entire-arm-exoskeleton-gives-stroke-patients-independence/ ';
+            news1.textContent = 'Now that 2025 is over we can look at some data. An estimated  38.1 billion tonnes of carbon dioxide has been released into the atmosphere as of 2025. As many already know going at such a pace is not sustainable, but where will we go to fix this is the ultimate question.';
+            news2.textContent = 'Mechanical exoskeletons are being made to help everyday people who suffered from stroke, regain motor fuctions. This technology has been developed in Norway and asking the test subjects they are quite positive on their effectiveness.';
         }
     }
 
